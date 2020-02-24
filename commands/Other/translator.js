b3c3125849e27d69be989a80da79aa6a183ecb30
mirko93s <mirko.1893@hotmail.it>
2020-02-24 03:34:09 +0100

    fixed
    Fixed a bug in a try catch block

--- commands/Other/translator.js
+++ commands/Other/translator.js
@@ -8,7 +8,7 @@ module.exports = {
     aliases: ["translate"],
     category: "Other",
     description: "Translate something",
-    usage: "[codelist or codes] <from> <to> <text>",
+    usage: "[no arguments returns language codes list] <from> <to> <text>",
     run: async (client, msg, arg) => {
         msg.delete();
 
@@ -16,6 +16,10 @@ module.exports = {
             .setColor(`RED`)
             .setTitle(`⛔ Please provide 2 different languages and the text to translate`)
             .setFooter(`<from> <to> <text> Example: en it Hello World. Leave them blank to get a link to code list`)
+        const errorEmbed = new Discord.RichEmbed()
+            .setColor(`RED`)
+            .setTitle(`⛔ Error: 400 Bad Request`)
+            .setFooter(`Something went wrong or probably you typed wrong codes.\nPlease try again.`)
 
         var codes = {
             "az": "Azerbaijan","sq": "Albanian","am": "Amharic","en": "English","ar": "Arabic","hy": "Armenian","af": "Afrikaans","eu": "Basque",
@@ -32,14 +36,17 @@ module.exports = {
         }
 
         const text = arg.slice(2).join(' ')
-        const to = arg[1]
         const from = arg[0]
+        const to = arg[1]
         const translatorcodesEmbed = new Discord.RichEmbed()
             .setColor(`RANDOM`)
-            .setDescription(`[**Languages Codes List**](https://github.com/mirko93s/Chill/wiki/Translator-Code-List)`);
-            if (!arg[0] && !arg[1] && !arg[2]) return msg.channel.send(translatorcodesEmbed).then(msg => msg.delete(5000));
-            if (arg[0] === arg[1] || !arg[2]) return msg.channel.send (noargsEmbed).then(msg => msg.delete(5000));
+            .setTitle(`🗣️ Translator`)
+            .setThumbnail(`https://camo.githubusercontent.com/f2ec5a009334a6dfafe485313689b137b8dbfd90/68747470733a2f2f692e696d6775722e636f6d2f34597a504270372e706e67`)
+            .setDescription(`Click on the image or open the link below\n\n[**Codes List**](https://github.com/mirko93s/Chill/wiki/Translator-Code-List)`);
 
+        if (!arg[0] && !arg[1] && !arg[2]) return msg.channel.send(translatorcodesEmbed).then(msg => msg.delete(30000));
+        if (arg[0] === arg[1] || !arg[2]) return msg.channel.send (noargsEmbed).then(msg => msg.delete(5000));
+        try {
             const { body } = await snekfetch
                 .get('https://translate.yandex.net/api/v1.5/tr.json/translate')
                 .query({
@@ -50,8 +57,13 @@ module.exports = {
             const lang = body.lang.split('-');
             const embed = new Discord.RichEmbed()
                 .setColor(0x00AE86)
+                .setTitle(`🗣️ Translator`)
                 .addField(`From: ${codes[lang[0]]}`, text)
                 .addField(`To: ${codes[lang[1]]}`, body.text[0]);
-            msg.channel.send(embed).catch(console.error);
+            msg.channel.send(embed).catch(err => console.log(err));
+        } catch (error) {
+            console.error(error);
+            msg.channel.send(errorEmbed).then(msg => msg.delete(5000));
+            }    
     }
 }
