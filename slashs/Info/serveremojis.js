35bf7ee87b826fc9cbd63c4291ffcd1879786510
mirko93s <mirko.1893@hotmail.it>
2022-02-21 14:54:55 +0100

    12.8.0

--- slashs/Info/serveremojis.js
+++ slashs/Info/serveremojis.js
@@ -9,12 +9,18 @@ module.exports = {
         const noemojiEmbed = new Discord.MessageEmbed()
             .setColor(`RED`)
             .setTitle(`⛔ Server has no emojis`)
+        const emojiEmbed = new Discord.MessageEmbed()
+            .setColor('RANDOM')
+            .setTitle('Server Emojis')
 
-        const emojis = await interaction.guild.emojis.cache.map((e) => `${e}`).join(' ').toString();
+        let emojis = await interaction.guild.emojis.cache.map((e) => `${e}`).join(' ').toString();
         if (emojis.length < 1) return interaction.reply({ephemeral:true, embeds:[noemojiEmbed]});
-
-        const embed = new Discord.MessageEmbed()
-            .setDescription(`Server Emojis:\n${emojis}`)
-        interaction.reply({embeds:[embed]});
+        else if (emojis.length > 4096) {
+            emojis = emojis.slice(0, 4096).split(' ');
+            if (!emojis[emojis.length-1].endsWith('>')) emojis = emojis.splice(0, emojis.length-1).join(' ');
+            embed.setFooter({text:'Some emojis are missing in this list due to Discord 4096 characters limit.'})
+        }
+        embed.setDescription(emojis.replace(/\s/g,''));
+        interaction.reply({embeds:[emojiEmbed]});
     }
 }
