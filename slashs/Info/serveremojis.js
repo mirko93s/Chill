a4b7ee17e9f6fd7829ae511b724bc42d8b013e91
mirko93s <mirko.1893@hotmail.it>
2022-02-21 15:11:52 +0100

    12.8.0

--- slashs/Info/serveremojis.js
+++ slashs/Info/serveremojis.js
@@ -18,9 +18,9 @@ module.exports = {
         else if (emojis.length > 4096) {
             emojis = emojis.slice(0, 4096).split(' ');
             if (!emojis[emojis.length-1].endsWith('>')) emojis = emojis.splice(0, emojis.length-1).join(' ');
-            embed.setFooter({text:'Some emojis are missing in this list due to Discord 4096 characters limit.'})
+            emojiEmbed.setFooter({text:'Some emojis are missing in this list due to Discord 4096 characters limit.'})
         }
-        embed.setDescription(emojis.replace(/\s/g,''));
+        emojiEmbed.setDescription(emojis.replace(/\s/g,''));
         interaction.reply({embeds:[emojiEmbed]});
     }
 }
